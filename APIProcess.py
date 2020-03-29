993ccb04f620aed0b842c2b3771c910d99e9e660
Joshua Shallow <josh.shallow@gmail.com>
2020-03-29 17:08:45 +0100

    Updating to include country comparison and single country statistics

--- APIProcess.py
+++ APIProcess.py
@@ -2,8 +2,21 @@ import requests
 import json
 import datetime
 import matplotlib.pyplot as plt
+
 rootRequest = "https://api.covid19api.com/"
 plt.style.use('seaborn')
+
+
+def formatList(list):
+    output = ""
+    if len(list) > 1:
+        for item in list[:len(list)-2]:
+            output += item + ", "
+        output += "and " + list[-1]
+    else:
+        output = list
+    return output
+
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
@@ -26,29 +39,45 @@ def countryStatus(country_slug, status):
 
 
 def parseData(countryJSON):
-    print(countryJSON)
-    dates = []
-    cases = []
+    caseDict = {}
     for object in countryJSON:
         caseDate = datetime.datetime(int(object['Date'][:4]), int(object['Date'][5:7]), int(object['Date'][8:10]))
         numberCases = int(object['Cases'])
-        dates.append(caseDate)
-        cases.append(numberCases)
-    return dates, cases
+        if caseDate in caseDict:
+            caseDict[caseDate] += numberCases
+        else:
+            caseDict[caseDate] = numberCases
+    return caseDict
 
 
 def plotGraph(dates, cases):
     plt.plot_date(dates, cases, linestyle='solid')
     plt.tight_layout()
     plt.gcf().autofmt_xdate()
-    plt.show()
+
+
+def singleCountryStats(country):
+    statuses = ['confirmed', 'recovered', 'deaths']
+    for status in statuses:
+        countryJSON = countryStatus(country, status)
+        cases = parseData(countryJSON)
+        plotGraph(cases.keys(), cases.values())
+    plt.legend(labels=statuses)
+
+
+def compareCounties(countries, status):
+    for country in countries:
+        countryJSON = countryStatus(country, status)
+        cases = parseData(countryJSON)
+        plotGraph(cases.keys(), cases.values())
+    plt.legend(labels=countries)
+    plt.title(status.capitalize() + " total for countries : " + formatList(countries))
 
 
 def main():
-    # helperFunction()
-    countryJSON = countryStatus("spain", "confirmed")
-    dates, cases = parseData(countryJSON)
-    plotGraph(dates, cases)
+    #singleCountryStats('italy')
+    compareCounties(['us', 'italy'], 'deaths')
+    plt.show()
 
 
 if __name__ == '__main__':
