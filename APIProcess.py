39a28765fa74165778c648762eb2cc47a8a688de
Joshua Shallow <josh.shallow@gmail.com>
2020-04-05 17:46:59 +0100

    Data request reformatted for UK and France.

--- APIProcess.py
+++ APIProcess.py
@@ -16,12 +16,42 @@ class Country:
         self.totals.append(count)
 
 
-def countryStatus(country_slug, status):
-    requestPath = ""
-    if status in {'confirmed', 'recovered', 'deaths'}:
-        requestPath = "{}country/{}/status/{}".format(constants.rootRequest, country_slug, status)
-    response = requests.get(requestPath)
-    return response.json()
+def countryDataRequest(country_slug, status):
+    '''
+    :param country_slug: country of the request from https://api.covid19api.com/countries
+    :param status: can be one of deaths, confirmed, recovered
+    :return: json of the requested country, mapped based on countryMapping function
+    '''
+    statuses = {'confirmed', 'recovered', 'deaths'}
+    if status not in statuses:
+        raise ValueError("Invalid status. Expected one of: %s" % statuses)
+
+    provinceRequest = ""
+    if country_slug in constants.UKSet:
+        provinceRequest = country_slug
+        country_slug = 'united-kingdom'
+    elif country_slug in constants.FranceSet:
+        provinceRequest = country_slug
+        country_slug = 'france'
+
+    requestPath = "{}country/{}/status/{}".format(constants.rootRequest, country_slug, status)
+    response = requests.get(requestPath).json()
+
+    if provinceRequest != "":
+        provinceResponse = []
+        for jsonObject in response:
+            province = jsonObject['Province'] if 'Province' in jsonObject else ""
+            if province.lower() == provinceRequest.lower():
+                provinceResponse.append(jsonObject)
+        response = provinceResponse
+    elif country_slug in constants.countriesWithProvinces:
+        countryResponse = []
+        for jsonObject in response:
+            province = jsonObject['Province'] if 'Province' in jsonObject else ""
+            if province == "":
+                countryResponse.append(jsonObject)
+        response = countryResponse
+    return response
 
 
 def parseData(countryJSON, ignoreZeroDates=False):
@@ -62,12 +92,12 @@ def plotGraph(dates, cases):
 
 
 def singleCountryStats(country, ignoreZeroDates=False):
-    #statuses = ['deaths', 'recovered', 'confirmed']
-    statuses = ['confirmed']
+    # statuses = ['deaths', 'recovered', 'confirmed']
+    statuses = ['deaths']
     countryCount = 0
     countryLegend = []
     for status in statuses:
-        countryJSON = countryStatus(country, status)
+        countryJSON = countryDataRequest(country, status)
         cases = parseData(countryJSON, ignoreZeroDates)
         for country in cases:
             plotGraph(country.dates, country.totals)
@@ -83,7 +113,7 @@ def singleCountryStats(country, ignoreZeroDates=False):
 
 def compareCounties(countries, status, ignoreZeroDates=False):
     for country in countries:
-        countryJSON = countryStatus(country, status)
+        countryJSON = countryDataRequest(country, status)
         cases = parseData(countryJSON, ignoreZeroDates)
         plotGraph(cases.keys(), cases.values())
     plt.legend(labels=countries)
@@ -92,8 +122,9 @@ def compareCounties(countries, status, ignoreZeroDates=False):
 
 
 def main():
-    singleCountryStats('united-kingdom', ignoreZeroDates=True)
+    singleCountryStats('gibraltar', ignoreZeroDates=True)
     # compareCounties(['us', 'italy', 'united-kingdom', 'france', 'germany', 'spain'], 'confirmed', ignoreZeroDates=True)
+    # helperFunctions.jprint(countryDataRequest('gibraltar', 'confirmed'))
 
 
 if __name__ == '__main__':
--- helperFunctions.py
+++ helperFunctions.py
@@ -1,4 +1,6 @@
 import json
+import requests
+import constants
 import sys
 
 
